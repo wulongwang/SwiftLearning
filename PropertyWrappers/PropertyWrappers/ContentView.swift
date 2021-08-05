b7ab84b1ce422e6c7af80d359c09777f89f85974
吴隆旺 <wulongwang@baijia.com>
2021-08-05 11:49:19 +0800

    Property Wappers demo

---
+++ PropertyWrappers/PropertyWrappers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PropertyWrappers
+//
+//  Created by Mac on 2021/8/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PropertyWrappers/PropertyWrappers/PropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersApp.swift
+//  PropertyWrappers
+//
+//  Created by Mac on 2021/8/5.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
