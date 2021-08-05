ce62f5efdc8877825f87b75194b322e73b46641f
吴隆旺 <wulongwang@baijia.com>
2021-08-05 18:00:48 +0800

    solid demo

---
+++ SOLID/SOLID/SOLIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOLIDApp.swift
+//  SOLID
+//
+//  Created by Mac on 2021/8/5.
+//
+
+import SwiftUI
+
+@main
+struct SOLIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
