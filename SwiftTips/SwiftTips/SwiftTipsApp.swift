404e1e5f381f46b0436ee3da3ea19e02abb173ee
wulongwang <wulongwang@baijia.com>
2021-08-06 09:43:24 +0800

    add swift tips

---
+++ SwiftTips/SwiftTips/SwiftTipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTipsApp.swift
+//  SwiftTips
+//
+//  Created by Mac on 2021/8/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
